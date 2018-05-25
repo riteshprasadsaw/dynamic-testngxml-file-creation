96c0dd13d94711acdddeb4604e91d30fb8bb4e4b
Ritesh <ryanisenia@hotmail.com>
2018-05-25 18:40:28 +0530

    first commit

--- jenkins_pipeline/E2E-automation-test.groovy
+++ jenkins_pipeline/E2E-automation-test.groovy
@@ -1,6 +1,6 @@
 #!groovy
 node {
-    #! parameters can be used to give info the stage section
+
     parameters {
         string(name: 'tomcat_dev', defaultValue: '35.166.210.154', description: 'Staging Server')
         string(name: 'tomcat_prod', defaultValue: '34.209.233.6', description: 'Production Server')
